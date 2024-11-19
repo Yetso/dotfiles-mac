6818d6ab6d86b24ad8cd2aad58864f57de21b5bb
Yetso <dav.catoul@gmail.com>
2024-11-19 13:36:45 +0100

    moved nix file to root folder dotfiles and removed --impure link

---
+++ flake.nix
@@ -0,0 +1,206 @@
+{
+  description = "Yetso Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nix-darwin, nixpkgs, nix-homebrew, home-manager }:
+    let
+      configuration = { pkgs, config, lib, ... }: {
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = with pkgs; [
+          cargo
+          # colima
+          # docker
+          podman
+          duti
+          fzf
+          git
+          gradle
+          lua
+          neovim
+          python312
+          python312Packages.pip
+          wget
+          zoxide
+          zulu
+        ];
+
+        environment.variables = {
+          EDITOR = "nvim";
+          LANG = "en_US.UTF-8";
+          HOMEBREW_NO_ANALYTICS = "1";
+          HOMEBREW_NO_ENV_HINTS = "1";
+          XDG_CONFIG_HOME = "/Users/yetso/.config";
+          _ZO_RESOLVE_SYMLINKS = "1";
+        };
+        environment.shells = [ pkgs.zsh ];
+
+        fonts.packages = with pkgs; [
+          (nerdfonts.override { fonts = [ "CommitMono" ]; })
+        ];
+
+        homebrew = {
+          enable = true;
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+          casks = [
+            "appcleaner"
+            "bitwarden"
+            "brave-browser"
+            "coteditor"
+            "cyberghost-vpn"
+            "discord"
+            "firefox"
+            "foxitreader"
+            "latest"
+            "onlyoffice"
+            "qbittorrent"
+            "skim"
+            "spotify"
+            "unnaturalscrollwheels"
+            "vlc"
+            { name = "wezterm@nightly"; }
+          ];
+        };
+
+        users.users.yetso.home = "/Users/yetso";
+        nix.configureBuildUsers = true;
+
+
+        system.defaults = {
+          ".GlobalPreferences"."com.apple.mouse.scaling" = 0.6875;
+          hitoolbox.AppleFnUsageType = "Do Nothing";
+          LaunchServices.LSQuarantine = false;
+          NSGlobalDomain = {
+            AppleEnableSwipeNavigateWithScrolls = true;
+            AppleInterfaceStyle = "Dark";
+            AppleMeasurementUnits = "Centimeters";
+            AppleMetricUnits = 1;
+            AppleShowAllExtensions = true;
+            AppleTemperatureUnit = "Celsius";
+            InitialKeyRepeat = 15;
+            KeyRepeat = 2;
+            NSAutomaticCapitalizationEnabled = false;
+            NSDisableAutomaticTermination = false;
+            NSScrollAnimationEnabled = false;
+            NSTableViewDefaultSizeMode = 1;
+            NSWindowResizeTime = 0.0;
+            "com.apple.keyboard.fnState" = true;
+            "com.apple.sound.beep.feedback" = 1;
+            "com.apple.springing.enabled" = false;
+            "com.apple.trackpad.forceClick" = false;
+          };
+          WindowManager.AppWindowGroupingBehavior = true;
+          WindowManager.EnableStandardClickToShowDesktop = false;
+          WindowManager.StandardHideDesktopIcons = false;
+          dock = {
+            autohide = true;
+            autohide-delay = 0.0;
+            autohide-time-modifier = 0.1;
+            expose-animation-duration = 0.3;
+            magnification = false;
+            mineffect = "scale";
+            minimize-to-application = true;
+            persistent-apps = [
+              "/Applications/Firefox.app"
+              "/System/Applications/Mail.app"
+              "/Applications/WezTerm.app"
+              "/System/Applications/Music.app"
+              "/Applications/Discord(Vesktop).app"
+            ];
+            show-recents = false;
+            tilesize = 48;
+            wvous-bl-corner = 11; #add launchpad shortcut on hotCorner
+          };
+          finder = {
+            AppleShowAllExtensions = true;
+            FXDefaultSearchScope = "SCcf";
+            FXPreferredViewStyle = "Nlsv";
+            _FXSortFoldersFirst = true;
+            ShowPathbar = true;
+          };
+          trackpad.ActuationStrength = 1;
+          trackpad.TrackpadRightClick = true;
+          universalaccess.mouseDriverCursorSize = 0.5;
+        };
+        time.timeZone = "Europe/Brussels";
+        system.startup.chime = false;
+
+        security.pam.enableSudoTouchIdAuth = true;
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.bash.enable = false;
+        programs.zsh = {
+          enable = true;
+          enableSyntaxHighlighting = true;
+          enableBashCompletion = false;
+          promptInit = "autoload -Uz promptinit && promptinit";
+        };
+        programs.fish.enable = false;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        nixpkgs.config.allowUnfree = true; # allow non open source application
+
+        system.stateVersion = 5;
+        # nix.extraOptions = ''
+        #   extra-platforms = x86_64-darwin aarch64-darwin
+        # '';
+
+        # Just to add JDK
+        system.activationScripts.extraActivation.text = "ln -sf '${pkgs.zulu}/zulu-21.jdk' '/Library/Java/JavaVirtualMachines/'";
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Yetso-laptop
+      darwinConfigurations."Yetso-laptop" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.yetso = import ./home.nix;
+          }
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              # enableRosetta = true;
+              # User owning the Homebrew prefix
+              user = "yetso";
+              # Automatically migrate existing Homebrew installations
+              # autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."Yetso-laptop".pkgs;
+    };
+}
---
+++ home.nix
@@ -0,0 +1,161 @@
+{ config, pkgs, ... }:
+
+let
+  homeDirectory = "/Users/yetso";
+  dotfiles = "${homeDirectory}/dotfiles";
+in {
+  xdg.enable = true;
+  home = {
+    username = "yetso";
+    homeDirectory = "${homeDirectory}";
+    stateVersion = "25.05";
+    file = {
+      ".config/fastfetch".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/fastfetch";
+      ".config/wezterm".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/wezterm/.config";
+      ".wezterm-completion.sh".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/wezterm/.wezterm-completion.sh";
+      ".terminfo".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/wezterm/.terminfo";
+      ".ssh/config".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/ssh/config";
+      ".config/starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/starship/starship.toml";
+      ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/nvim";
+    };
+    sessionVariables = {
+      HOMEBREW_NO_ANALYTICS = "1";
+      HOMEBREW_NO_ENV_HINTS = "true";
+      XDG_CONFIG_HOME = "/Users/yetso/.config";
+      _ZO_RESOLVE_SYMLINKS = "1";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    # autosuggestion.stategy = [        # not available yet
+    #   "completion"
+    # ];
+    # syntaxHighlighting.enable = true;     #activated with flake.nix on a system level
+    enableCompletion = false;   # disabled because already there for system shell
+    loginExtra = "fastfetch";
+    shellAliases = {
+      cat = "bat";
+      lt = "ls --long --tree --level=3 --ignore-glob='.git'";
+      fastfetch = "clear;command fastfetch";
+      lg = "lazygit";
+      v = "nvim";
+      docker = "podman";
+      update = "\
+        open /Applications/Latest.app
+        brew update --quiet
+        brew upgrade --quiet
+        brew upgrade --quiet --cask wezterm@nightly --no-quarantine --greedy-latest
+        echo -e '\\e[34m==>\\e[0m \\e[1mupdating flake lock...\\e[0m'
+        (cd ~/dotfiles && nix flake update --commit-lock-file > /dev/null)
+        echo -e '\\e[34m==>\\e[0m \\e[1m rebuilding nix-darwin...\\e[0m'
+        darwin-rebuild switch --flake ~/dotfiles --impure > /dev/null
+      ";
+    };
+  };
+
+  programs.fish = {
+    enable = false;
+    shellAbbrs = {
+      lg = "lazygit";
+      v = "nvim";
+      darwin-rebuild = "darwin-rebuild switch --flake $(readlink -f ~/.config/nix-darwin)";
+    };
+    functions = {
+      cat = "bat $argv";
+      ls = "eza --all --header --binary --color=always --group-directories-first --icons=always --ignore-glob='.DS_Store' --no-quotes $argv";
+      lt = "eza --long --tree --level=3 --all --header --binary --color=always --group-directories-first --icons=always --ignore-glob='.DS_Store|.git' --no-quotes $argv";
+      fastfetch = "clear;command fastfetch $argv";
+      fish_greeting = "fastfetch";
+      update = "
+        set -l original_dir (pwd)
+        open /Applications/Latest.app
+        brew update
+        brew upgrade
+        brew upgrade --cask wezterm@nightly --no-quarantine --greedy-latest
+        cd ~/dotfiles/nix/.config/nix-darwin/
+        nix flake update --commit-lock-file
+        darwin-rebuild switch --flake . --impure
+        cd $original_dir
+      ";
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = false;
+    enableZshIntegration = true;
+    enableIonIntegration = false;
+    enableNushellIntegration = false;
+    # settings = pkgs.lib.importTOML config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/starship/starship.toml";
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = false;
+    enableFishIntegration = false;
+    enableZshIntegration = true;
+    enableNushellIntegration = false;
+    options = ["--cmd cd"];
+  };
+
+  programs.lazygit = {
+    enable = true;
+    settings.gui = {
+      theme.selectedLineBgColor = [ "#1c1c1c" ];
+      nerdFontsVersion = 3;
+    };
+  };
+
+  programs.fastfetch = {
+    enable = true;
+    # settings = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/fastfetch/fastfetchConfig.jsonc";
+  };
+
+  programs.eza = {
+    enable = true;
+    icons = "always";
+    extraOptions = [
+      "--binary"
+      "--group-directories-first"
+      "--header"
+      "--no-quotes"
+      "--all"
+      "--color=always"
+      "--ignore-glob=.DS_Store"
+      # lt = "eza --long --tree --level=3 --ignore-glob='.DS_Store|.git'";
+    ];
+  };
+
+  programs.bat = {
+    enable = true;
+    config.theme = "fly16";
+    themes = {
+      fly16 = {
+        src = pkgs.fetchFromGitHub {
+          owner = "bluz71";
+          repo = "fly16-bat"; # Bat uses sublime syntax for its themes
+          rev = "d13c2c2a03e84819b2df2c50bc824b74604b9844";
+          hash = "sha256-QtZurXK6wNnyWiiQWfyAMFsV5ZWyi84jJNkTgZYoMCE=";
+        };
+        file = "fly16.tmTheme";
+      };
+    };
+  };
+
+  # home.packages = [
+  
+  # ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  programs.git = {
+    enable = true;
+    userName = "Yetso";
+    userEmail = "dav.catoul@gmail.com";
+  };
+}
